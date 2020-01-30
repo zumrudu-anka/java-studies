376a918a3d1911cf2126379f85334a6b42edec8c
Zümrüd-ü Anka <osmandurdag@hotmail.com>
2020-01-30 19:43:12 +0300

    initial Commit

---
+++ DataTypes/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        byte number = 127;
+        String city = "Ankara";
+        char character = 'A';
+        boolean isTrue = true;
+        short number2 = 32767;
+    }
+}
---
+++ Variables/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int number = 10;
+        String mesaj = "Öğrenci Sayısı: ";
+	    System.out.println(mesaj + number);
+    }
+}
